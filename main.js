10eb3698819279ed4d409ec35479a8d637fc336f
MateusKotryk <mateus.kotryk.souza@escola.pr.gov.br>
2026-05-07 13:30:18 +0000

    aula 2

--- main.js
+++ main.js
@@ -15,3 +15,15 @@ for(let i=0;i <botoes.length;i++){
         botoes[i].classList.add("ativo");
     }
 }
+for ([inicialização]; [condição]; [incremento]) {
+  declaração;
+}for (var i = 1; i <= 5; i++) {
+  console.log(i);
+}
+for (var i = 0; i <= 10; i += 2) {
+  console.log(i);
+}
+var frutas = ["Maçã", "Banana", "Laranja", "Pera"];
+for (var i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+}
